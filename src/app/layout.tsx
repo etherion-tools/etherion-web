f6b538bc642d17ea60d91eccdfb5f67acfb012c5
Jack Ulf <metanodedreamer@gmail.com>
2025-07-21 20:30:48 -0600

    Initialize repo for etherion-web

---
+++ src/app/layout.tsx
@@ -0,0 +1,62 @@
+import type { Metadata } from "next";
+import { Inter, Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/provider/theme-provider";
+import { NavigationBar } from "@/components/common/NavigationBar";
+
+import Footer from '@/components/common/Footer';
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Etherion Tools",
+  description: "Web3 innovation through elegant and powerful developer tools",
+  icons: {
+    icon: "/etherion-tools-logo.svg",
+    shortcut: "/etherion-tools-logo.svg",
+    apple: "/etherion-tools-logo.svg",
+  },
+  openGraph: {
+    images: ["/etherion-tools-logo.svg"],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased dark:bg-[#101828]`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavigationBar />
+          <main className="pt-32">
+            {children}
+          </main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
